eeb853b0a93f505d3ea2b12f6e479c8f31bab078
Timothy Cruysberghs <tcr@aurelium.be>
2019-10-22 16:28:58 +0200

    Update Aurelium_tests.ps1

--- Aurelium_tests.ps1
+++ Aurelium_tests.ps1
@@ -6,8 +6,7 @@ class Tests
     Tests()
     {
       $this.oTests = new-object System.Xml.XmlDocument
-      
-      
+
       $decl = $this.oTests.CreateXmlDeclaration("1.0","UTF-8",$null)
       $this.oTests.AppendChild($decl)
       $this.oTests.createNode("element","Testen",$null)
@@ -24,13 +23,13 @@ class Tests
 
     }
     [void] saveWerkbonOutput([string]$path){
-
+        #Save and Open to text file
     }
-    [void] addTestResult([string]$name, [string]$servername, [boolean]$ok){
+    [void] addTestResult([string]$name, [string]$servername, [String]$Value, [boolean]$ok){
         
     }
 
-    [void] getNableOutput(){
+    [void] getMonitoringOutput(){
 
     }
 }
