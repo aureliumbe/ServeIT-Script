580d65a3b8b919cbc49ffcc07dda2eecf4a5d46e
Timothy Cruysberghs <tcr@aurelium.be>
2019-10-22 15:01:00 +0200

    Create Aurelium_tests.ps1

---
+++ Aurelium_tests.ps1
@@ -0,0 +1,36 @@
+class Tests
+{
+#Properties
+    [XML] $oTests
+#constructor
+    Tests()
+    {
+      $this.oTests = new-object System.Xml.XmlDocument
+      
+      $decl = $this.oTests.CreateXmlDeclaration("1.0","UTF-8",$null)
+      $this.oTests.AppendChild($decl)
+      $this.oTests.createNode("element","Testen",$null)
+    }
+
+#Functions and methods
+    [void] outputByServer(){
+
+    }   
+    [void] outputByTest(){
+
+    } 
+    [void] saveXml([string]$path){
+
+    }
+    [void] saveWerkbonOutput([string]$path){
+
+    }
+    [void] addTestResult([string]$name, [string]$servername, [boolean]$ok){
+        
+    }
+
+    [void] getNableOutput(){
+
+    }
+}
+
