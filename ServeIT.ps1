aureliumbe/ServeIT-Script

commit d33268ead5235651cad9251c59623e705ae96912

Update ServeIT.ps1
added SQL 2022 and a default value to Aget-SQL_Server_Instances.
Updated version to 1.8Beta49-123

ServeIT.ps1
@@ -168,7 +168,7 @@ if (-Not $isAdmin) {
 ########################################
 #### Display ServeIT Script Version ####
 ########################################
-Write-Host "Version 1.8Beta49-122" -ForegroundColor Yellow | Out-Default
+Write-Host "Version 1.8Beta49-123" -ForegroundColor Yellow | Out-Default
 
 
 #############################################
@@ -4811,6 +4811,7 @@ else {
 
         #$SQL_Instance_Properties.PatchLevel.split(".")[0]
         Switch ($SQL_Instance_Properties.PatchLevel.split(".")[0]) {
+            "16" {$ReturnMsg = "Version: 2022 - "+$SQL_Instance_Properties.Edition+" installed"}
             "15" {$ReturnMsg = "Version: 2019 - "+$SQL_Instance_Properties.Edition+" installed"}
             "14" {$ReturnMsg = "Version: 2017 - "+$SQL_Instance_Properties.Edition+" installed"}
             "13" {$ReturnMsg = "Version: 2016 - "+$SQL_Instance_Properties.Edition+" installed"}
@@ -4825,6 +4826,7 @@ else {
             "9" {$ReturnMsg = "Version: 2005 - "+$SQL_Instance_Properties.Edition+" installed"}
             "8" {$ReturnMsg = "Version: 2000 - "+$SQL_Instance_Properties.Edition+" installed"}
             "7" {$ReturnMsg = "Version: 7.0 - "+$SQL_Instance_Properties.Edition+" installed"}
+	    Default {$ReturnMsg = "Version: "$SQL_Instance_Properties.PatchLevel+" - "+$SQL_Instance_Properties.Edition+" installed"}
             }
         $ReturnValue += ACreate-TestResult "SQL Server" $ReturnMsg $true    
         }

commit 3869df1c2512e4977eda7e51410211b388847010

Fixed issue with string in default case in SQL check
issue was introduced in Commit https://github.com/aureliumbe/ServeIT-Script/commit/d33268ead5235651cad9251c59623e705ae96912

ServeIT.ps1
@@ -4826,7 +4826,7 @@ else {
             "9" {$ReturnMsg = "Version: 2005 - "+$SQL_Instance_Properties.Edition+" installed"}
             "8" {$ReturnMsg = "Version: 2000 - "+$SQL_Instance_Properties.Edition+" installed"}
             "7" {$ReturnMsg = "Version: 7.0 - "+$SQL_Instance_Properties.Edition+" installed"}
-	    Default {$ReturnMsg = "Version: "$SQL_Instance_Properties.PatchLevel+" - "+$SQL_Instance_Properties.Edition+" installed"}
+	    Default {$ReturnMsg = "Version: "+$SQL_Instance_Properties.PatchLevel+" - "+$SQL_Instance_Properties.Edition+" installed"}
             }
         $ReturnValue += ACreate-TestResult "SQL Server" $ReturnMsg $true    
         }