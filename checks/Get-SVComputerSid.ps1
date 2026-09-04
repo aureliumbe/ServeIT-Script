8fd2338b68b3827b0aba89b1138715fa971ad2d4
TimonTrukker <16043821+TimonTrukker@users.noreply.github.com>
2026-09-04 12:28:29 +0200

    SID was niet leesbaar. Nu zouden ook duplicates moeten worden gedetecteerd.

--- checks/Get-SVComputerSid.ps1
+++ checks/Get-SVComputerSid.ps1
@@ -1,15 +1,21 @@
 function Get-SVComputerSid($comp) {
 #
-# Get SID of a DOmain Joined Computer
+# Get SID of a Dmain Joined Computer
 #
     $ReturnValue = @()
 
-    $ReturnSID = get-adcomputer $comp -prop sid
-    $ReturnMsg = $ReturnSID.SID
+    $ReturnSID = get-adcomputer $comp -Properties sid
+    $ReturnMsg = $ReturnSID.SID.ToString()
 
-    #if ($vergelijker -contains $newsid) {Out-Default "gevonden"} else {$vergelijker += $newsid}
+    $SidCount = Get-ADComputer -Filter * -Properties SID | ? -Property SID -eq $ReturnMsg | ? -Property DistinguishedName -notlike "*OU=Domain Controllers*" | Measure-Object | Select-Object -ExpandProperty Count
+    if ($SidCount -gt 1) {
+        $ReturnMsg = $ReturnMsg + " (SID is not unique, found on $SidCount computers)"
+        $testvalue = $false
+    } else {
+        $testvalue = $true
+    }
 
-    $ReturnValue += New-SVTestResult "Computer SID" $ReturnMsg $true
+    $ReturnValue += New-SVTestResult "Computer SID" $ReturnMsg $testvalue
 
 return New-SVTest "Computer Unique SID" $ReturnValue
 }
